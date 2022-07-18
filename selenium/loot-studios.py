cdbab11fd3bfc10e59e89431b2fa9ae30f615f89
Guus van de Steeg <gv4@sanger.ac.uk>
2022-07-18 13:33:10 +0100

    black

--- selenium/loot-studios.py
+++ selenium/loot-studios.py
@@ -1,16 +1,11 @@
-import asyncio
 import os
 import shutil
-import time
 import urllib.request as urlrequest
-from pprint import pprint
-from time import sleep
 from urllib.error import URLError
 
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 
 FOLDER = "downloads"
 TO_SKIP = "to_skip.txt"
@@ -25,6 +20,7 @@ async def download(url, filename):
     except URLError as err:
         print(filename, "failed")
 
+
 try:
     with open(TO_SKIP) as f:
         to_skip = {l.strip() for l in f}
@@ -34,26 +30,37 @@ except FileNotFoundError:
 with webdriver.Chrome() as driver:
     driver.get("https://www.loot-studios.com/login")
     driver.find_element(value="member_email").send_keys("guus.steeg@gmail.com")
-    driver.find_element(value="member_password").send_keys(os.environ["PASSWORD"].strip())
+    driver.find_element(value="member_password").send_keys(
+        os.environ["PASSWORD"].strip()
+    )
     driver.find_element(By.NAME, "commit").click()
 
     i = 1
     while i <= 3:
-        driver.get(f'https://www.loot-studios.com/library?page={i}')
-        products = {l.get_attribute("href") for l in driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div").find_elements(By.TAG_NAME, "a")}
+        driver.get(f"https://www.loot-studios.com/library?page={i}")
+        products = {
+            l.get_attribute("href")
+            for l in driver.find_element(
+                By.XPATH, "/html/body/div[1]/div/div[3]/div"
+            ).find_elements(By.TAG_NAME, "a")
+        }
         for product in products:
             if "library" in product or product in to_skip:
                 continue
 
             driver.get(product)
-            mainfolder = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[3]/div/div/div/div/div/div/h1').text
+            mainfolder = driver.find_element(
+                By.XPATH, "/html/body/div[1]/div/div/div[3]/div/div/div/div/div/div/h1"
+            ).text
             if mainfolder.lower() != "welcome pack":
                 mainfolder = " ".join(mainfolder.strip().split()[:-1])
             mainfolder = os.path.join(FOLDER, mainfolder)
             os.makedirs(mainfolder, exist_ok=True)
 
             categories = set()
-            syllabus = driver.find_element(By.XPATH, '//*[@id="section-product_syllabus"]/div/div')
+            syllabus = driver.find_element(
+                By.XPATH, '//*[@id="section-product_syllabus"]/div/div'
+            )
             for element in syllabus.find_elements(By.CLASS_NAME, "syllabus__item"):
                 for href in element.find_elements(By.TAG_NAME, "a"):
                     if href.text != "Show More":
@@ -64,7 +71,9 @@ with webdriver.Chrome() as driver:
                 driver.get(category)
 
                 try:
-                    downloads = driver.find_element(By.CLASS_NAME, "downloads").find_elements(By.TAG_NAME, "a")
+                    downloads = driver.find_element(
+                        By.CLASS_NAME, "downloads"
+                    ).find_elements(By.TAG_NAME, "a")
                 except NoSuchElementException:
                     continue
 
@@ -76,7 +85,9 @@ with webdriver.Chrome() as driver:
                     if not d.text.endswith("zip"):
                         continue
                     with open("run.sh", "a") as f:
-                        f.write(f'wget "{d.get_attribute("href")}" -O "{path}/{d.text}" &\n')
+                        f.write(
+                            f'wget "{d.get_attribute("href")}" -O "{path}/{d.text}" &\n'
+                        )
                     # to_download.append((d.get_attribute("href"), os.path.join(path, d.text)))
 
             with open(TO_SKIP, "a") as f:
